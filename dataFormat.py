cbe2424458e58b28cde4112556c4a77f5d52f442
haig <haigharutunian@protonmail.com>
2022-06-14 16:48:27 -0400

    got conversion to csv
    conversion to csv done, need to find a way to search through the data and find specific things, and create gui and add menu to select item and it has all the nutrition facts to it

--- dataFormat.py
+++ dataFormat.py
@@ -1,11 +1,39 @@
 import pandas as pd
-import numpy as np
-import PyPDF2
+from pandas import *
 import tabula
+import csv
 
 
-
+print("Converting data please wait.")
 file_path = ".\Qdoba_Nutrition_Information_2021.pdf"
-
+pd.options.display.max_columns = None
 df = tabula.read_pdf(file_path, pages='all')
-print(df)
+
+tabula.convert_into(file_path, 'qdobaconverted.csv', output_format='csv', pages = 'all')
+
+#data = read_csv("qdobaconverted.csv")
+
+with open('qdobaconverted.csv') as csv_file:
+     csv_reader = csv.reader(csv_file)
+     dataf = pd.DataFrame([csv_reader], index = None, )
+
+print(dataf)
+
+
+
+print("Data has been converted.")
+
+
+
+for val in list(dataf[2]):
+    print(val)
+#with open('.\Qdoba_Nutrition_Information_2021.pdf', 'rb') as f:
+#    reader = PdfFileReader(f)
+#    contents = reader.getPage(0).extractText().split()
+#    pass
+
+#print('tabula output')
+#print(df)
+
+#print('pypdf2 output---------------------------------------------------------------')
+#print(contents)
