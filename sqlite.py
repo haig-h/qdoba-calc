0e863f048cfa737c2abb1a1fbc3c2a3822674af5
haig <haigharutunian@protonmail.com>
2022-06-20 15:13:39 -0400

    DB working
    panda creates database with foods and adds row names

--- sqlite.py
+++ sqlite.py
@@ -1,9 +1,7 @@
 import sqlite3
-import csv
 import pandas as pd
 
-file = "testing.db"
-
+file = "food.db"
 
 try:
 
@@ -11,19 +9,11 @@ try:
     print("Connected to SQlite")
 
     cursor = sqliteConnection.cursor()
-    print("test1")
 
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS testing5(foodname TEXT, allergens int, serving_size int, calories_p_serving int, calories_f_fat int, total_fat int, saturated_fat int,'
-        'trans_fat int, cholesterol int, sodium int, potassium int, total_carbs int, dietary_fiber int, sugar int, protein int);')
-    print('test2')
-    sqliteConnection.commit()
+    df = pd.read_csv('qdobaconverted.csv', names=['food', 'allergens', 'serving size(g)', 'calories per serving', 'calories in fat', 'total fat(g)', 'saturated fat(g)', 'trans fat(g)', 'cholesterol(mg)', 'sodium(mg)', 'potassium(mg)',
+                'total carbs(g)', 'dietary fiber(g)', 'sugar(g)', 'protein(g)'] ,encoding_errors='replace', on_bad_lines='skip')
+    df.to_sql('foods', sqliteConnection, if_exists='replace', index=True)
 
-    df = pd.read_csv('qdobaconverted.csv', encoding_errors='replace', on_bad_lines='skip')
-    df.to_sql('testing5', sqliteConnection, if_exists='replace', index=True)
-    df.columns=['food', 'allergens', 'serving size', 'calories per serving', 'calories in fat', 'total fat', 'saturated fat', 'trans fat', 'cholesterol', 'sodium', 'potassium',
-                'total carbs', 'dietary fiber', 'sugar', 'protein']
-    print('test3')
     sqliteConnection.commit()
 
 except sqlite3.Error as error:
